8ab89bd6a7cc692e377e37bf7fc49c5f3823e6ac
solarballsbangla55-sys <solarballsbangla55@gmail.com>
2025-11-30 19:32:53 +0530

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -1,1 +1,11 @@
-print("Your server setup is starting...")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Your video translation server is running!"
+
+if __name__ == "__main__":
+    app.run()
+  
