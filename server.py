3c7d41b234b1d9f59034da48cfae61d45a9ef514
solarballsbangla55-sys <solarballsbangla55@gmail.com>
2025-12-09 13:40:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -6,6 +6,12 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 def home():
     return "<h1>Server working!</h1>"
+
+@app.post("/upload-video")
+async def upload_video(file: UploadFile = File(...)):
+    # For now, we only test upload works
+    return {"filename": file.filename, "status": "video uploaded successfully"}
+    
     
 
   
